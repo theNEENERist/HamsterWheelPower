bb8bfaff39687f170219f0c58b086430ce3ba275
MjwTurpin <151603888+MjwTurpin@users.noreply.github.com>
2026-06-24 02:19:36 -0300

    Added Sprites and Updated Char Movement
    Sprites for the PC and Wheel were added. Player can now move and do a little hop. Added a signal for when we need to interact, but it might not be the right fit for what we need.

---
+++ Scripts/player_character.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -100.0
+
+signal action_interact
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("Interact"):
+		emit_signal("action_interact")
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("Left", "Right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+
+	move_and_slide()
