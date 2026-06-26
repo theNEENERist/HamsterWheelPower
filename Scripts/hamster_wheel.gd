6faeb135eb9eded51ec3dc68a234191b4bb2ae9e
Joel Leach <joel.b.leach@gmail.com>
2026-06-26 13:37:40 -0400

    improved wheel functionality. added wheel on and off and loading bar.  removed debugging stuffs

--- Scripts/hamster_wheel.gd
+++ Scripts/hamster_wheel.gd
@@ -1,26 +1,18 @@
-extends AnimatedSprite2D
+extends Area2D
 
-@export var speed = 400
+@export var tooltip_text: String = "E"
 
-var wheel_selected: bool = true
-var screen_size # Size of the game window.
+var tooltip_label: Label
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	screen_size = get_viewport_rect().size
+func _ready() -> void:
+	pass # Replace with function body.
 
+# shows tooltip when the player is colliding with the wheel.
+func tooltip_show() -> void:
+	$"ToolTipLabel".text = tooltip_text
+	$"ToolTipLabel".show()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var wheel_velocity = Vector2.ZERO # The player's movement vector.
-	if wheel_selected:
-		if Input.is_action_pressed("ui_right"):
-			wheel_velocity.x += 1
-		elif Input.is_action_pressed("ui_left"):
-			wheel_velocity.x -= 1
-
-		if wheel_velocity.length() > 0:
-			wheel_velocity = wheel_velocity.normalized() * speed
-
-		position += wheel_velocity * delta
-		position = position.clamp(Vector2.ZERO, screen_size)
+ # hides the tooltip if player is not colliding with the wheel.
+func tooltip_hide() -> void:
+	$"ToolTipLabel".hide()
--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,14 +1,32 @@
 extends CanvasLayer
 
+@export var max_power: int = 25
+@export var min_power: int = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$"ProgressBar".max_value = max_power
+	$"ProgressBar".min_value = min_power
+	$ProgressBar.value = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+# updates the score label on the HUD with the current score
 func set_score(score: int) -> void:
-	$PowerLabel.text = str(score)
+	$ProgressBar.value = score
+
+# updates if max or min power has been reached absed on the value
+func _on_progress_bar_value_changed(value: float) -> void:
+	if value >= max_power:
+		Globals.max_power_reached = true
+		Globals.min_power_reached = false
+	elif value <= min_power:
+		Globals.min_power_reached = true
+		Globals.max_power_reached = false
+	else:
+		Globals.max_power_reached = false
+		Globals.min_power_reached = false
